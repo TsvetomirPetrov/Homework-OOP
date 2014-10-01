b6ba2cc7726f53add036f2a0a32a79773c4fcf3f
TsvetomirPetrov <petrov.tsvetomir1@gmail.com>
2014-10-01 18:35:15 +0300

    sixth

---
+++ 06.Homework-Functional-Programming/StringBuilderExtensions/TestStringBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+class TestStringBuilderExtensions
+{
+    static void Main()
+    {
+        var str = new StringBuilder("Functional Programming");
+        Console.WriteLine(str.Substring(0, 11));
+        str = new StringBuilder("Functional funcTional Programming");
+        Console.WriteLine(str.RemoveText("Functional"));
+        Console.WriteLine(str.AppendAll(new string[] { " ", "one", " ", "two" }));
+    }
+}
+
